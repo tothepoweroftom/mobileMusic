cec74c7a5514ffcfef9166ba72bc3a3b4aafea2f
tothepoweroftom <powerth@tcd.ie>
2016-09-23 15:08:25 +0100

    added edge detection

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+//get canvas
+var photoCanvas = document.getElementById("capturedPhoto");
+var ctx = photoCanvas.getContext("2d");
+
 function picChange(evt){
 //bring selected photo in
 
@@ -14,14 +18,15 @@ var windowURL = window.URL || window.webkitURL;
 //If the createObjectURL method is not supported by the user’s browser, this code will fail.
 var picURL = windowURL.createObjectURL(fileInput[0]);
 
-//get canvas
-var photoCanvas = document.getElementById("capturedPhoto");
-var ctx = photoCanvas.getContext("2d");
+
 var photo = new Image();
 
 photo.onload = function(){
   //draw photo into canvas when ready
-  ctx.drawImage(photo, 0, 0, 500, 400);
+  ctx.drawImage(photo, 0, 0, photoCanvas.width, photoCanvas.height);
+      var originalData = ctx.getImageData(0, 0, photoCanvas.width, photoCanvas.height);
+      findEdges(originalData);
+
 };
 
 //load photo into canvas
@@ -30,4 +35,38 @@ photo.src = picURL;
 windowURL.revokeObjectURL(picURL);
 
 }
+}
+
+function findEdges(originalData) {
+    var output = ctx.createImageData(photoCanvas.width, photoCanvas.height);
+    var w = originalData.width, h = originalData.height;
+    var inputData = originalData.data;
+    var outputData = output.data;
+    var threshold = 100;
+    // edge detection goes here
+  // edge detection
+  for (var y = 0; y < h; y += 1) {
+    for (var x = 0; x < w; x += 1) {
+      var i = (y * w + x) * 4;
+        outputData[i] = inputData[i - w*4 - 4]  +
+                        inputData[i - w*4]  + inputData[i - w*4 + 4] +
+                        inputData[i - 4] - 8 * inputData[i] + inputData[i + 4] +
+                        inputData[i + w*4 - 4] + inputData[i + w*4] + inputData[i + w*4 + 4];
+        if (outputData[i] < threshold)
+        {
+         outputData[i] = 255;
+         outputData[i+1] = 255;
+         outputData[i+2] = 255;
+        } else {
+         outputData[i] = 0;
+         outputData[i+1] = 0;
+         outputData[i+2] = 0;
+        }
+         outputData[i + 3] = 255; // alpha
+   }
+ }
+
+
+    // put the image data back after manipulation
+    ctx.putImageData(output, 0, 0);
 }
